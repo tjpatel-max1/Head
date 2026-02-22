03d5bc99bae04853c8369972ac8a7b9872b719d1
tjpatel-max1 <tjpatel990901@gmail.com>
2026-02-22 23:16:36 +0530

    Update keepalive.py

--- modules/keepalive.py
+++ modules/keepalive.py
@@ -1,1 +1,19 @@
+from flask import Flask
+import os
+import threading
 
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Bot is alive ✅"
+
+
+def run():
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port)
+
+
+def keep_alive():
+    t = threading.Thread(target=run)
+    t.start()
